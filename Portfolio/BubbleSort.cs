e4f1d719f64aed751fac0e35b0dfcabb2c9febb7
Jordan <JordanChell@vivaldi.net>
2020-05-17 11:23:35 +0100

    Added testing var, and starting bubble sort

--- Portfolio/BubbleSort.cs
+++ Portfolio/BubbleSort.cs
@@ -9,6 +9,29 @@ namespace Portfolio
         public int[] unsortedList { get; set; }
         public int[] sortedList { get; set; }
 
+        public void sort(int reccursion) 
+        {
+            reccursion--;
+            int currentVal, nextVal, tempVal;
+            if(reccursion >= 0) 
+            {             
+                for(int i = 0; i < unsortedList.Length-1; i++) 
+                {
+                    currentVal = unsortedList[i];
+                    nextVal = unsortedList[i+1];
+                
+                    if(currentVal>nextVal) 
+                    {
+                        tempVal = nextVal;
+                        unsortedList[i + 1] = currentVal;
+                        unsortedList[i] = tempVal;
+                    }
+                }
+                sort(reccursion);
+            }
+
+        }
+
         
     }
 }
--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -6,76 +6,92 @@ namespace Portfolio
     {
         static void Main(string[] args)
         {
+            int testing = 0; //Change this to change what gets tested
+
             Stack myStack = new Stack();
             Queue myQueue = new Queue();
             LinkedList myLinkedList = new LinkedList();
             DoubleLinkedList myDoubleLinkedList = new DoubleLinkedList();
             BubbleSort myBubbleSort = new BubbleSort();
 
-
             //Stack testing
-            //Console.WriteLine("Stack Testing: ");
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine("Please input a node value to add to the stack: ");
-            //    string UserInput = Console.ReadLine();
-            //    myStack.Push(CreateNode(UserInput));
-            //    Console.WriteLine("Node added");
+            if(testing == 0)
+            {
+                Console.WriteLine("Stack Testing: ");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Please input a node value to add to the stack: ");
+                    string UserInput = Console.ReadLine();
+                    myStack.Push(CreateNode(UserInput));
+                    Console.WriteLine("Node added");
 
-            //}
+                }
 
-            //myStack.print();
-            //myStack.Pull();
-            //myStack.print();
-
+                myStack.print();
+                myStack.Pull();
+                myStack.print();
+            }
 
             //Queue testing
-            //Console.WriteLine("Queue Testing: ");
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine("Please input a node value to add to the queue: ");
-            //    string UserInput = Console.ReadLine();
-            //    myQueue.Enqueue(CreateNode(UserInput));
-            //    Console.WriteLine("Node Added");
-            //}
+            else if(testing == 1) 
+            { 
+                Console.WriteLine("Queue Testing: ");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Please input a node value to add to the queue: ");
+                    string UserInput = Console.ReadLine();
+                    myQueue.Enqueue(CreateNode(UserInput));
+                    Console.WriteLine("Node Added");
+                }
 
-            //myQueue.print();
-            //myQueue.Dequeue();
-            //myQueue.print();
+                myQueue.print();
+                myQueue.Dequeue();
+                myQueue.print();            
+            }
 
+            else if(testing == 2) 
+            { 
+                //Linked list testing
+                Console.WriteLine("LL testing: ");
+                //Creating nodes to add to list
+                myLinkedList.appendStart(CreateNode("3"));
+                myLinkedList.appendStart(CreateNode("8"));
+                myLinkedList.appendStart(CreateNode("4"));
+                myLinkedList.appendStart(CreateNode("5"));
+                myLinkedList.appendEnd(CreateNode("7"));
 
-            //Linked list testing
-            Console.WriteLine("LL testing: ");
-            //Creating nodes to add to list
-            myLinkedList.appendStart(CreateNode("3"));
-            myLinkedList.appendStart(CreateNode("8"));
-            myLinkedList.appendStart(CreateNode("4"));
-            myLinkedList.appendStart(CreateNode("5"));
-            myLinkedList.appendEnd(CreateNode("7"));
+                Console.WriteLine("Before removal calls");
+                myLinkedList.print();
 
-            Console.WriteLine("Before removal calls");
-            myLinkedList.print();
+                //myLinkedList.removeStart();
+                //myLinkedList.removeEnd();
 
-            //myLinkedList.removeStart();
-            //myLinkedList.removeEnd();
+                myLinkedList.append(CreateNode("11"), 1); //Change the int here to change where the value gets appended
+                myLinkedList.remove(7);
 
-            myLinkedList.append(CreateNode("11"), 1); //Change the int here to change where the value gets appended
-            myLinkedList.remove(7);
+                Console.WriteLine("After removal calls");
+                myLinkedList.print();            
+            }
 
-            Console.WriteLine("After removal calls");
-            myLinkedList.print();
+            else if(testing == 3) 
+            { 
+                //DoubleLinkedList Testing
+                Console.WriteLine("DoubleLinkedList testing: ");
+                myDoubleLinkedList.appendStart(CreateNode("5"));
+                myDoubleLinkedList.appendStart(CreateNode("7"));
+                myDoubleLinkedList.appendStart(CreateNode("4"));
+                myDoubleLinkedList.append(CreateNode("11"), 1);
+                myDoubleLinkedList.appendEnd(CreateNode("8"));
+                myDoubleLinkedList.print();
+                myDoubleLinkedList.remove(5);
+                myDoubleLinkedList.print();           
+            }
 
-            //DoubleLinkedList Testing
-            Console.WriteLine("DoubleLinkedList testing: ");
-            myDoubleLinkedList.appendStart(CreateNode("5"));
-            myDoubleLinkedList.appendStart(CreateNode("7"));
-            myDoubleLinkedList.appendStart(CreateNode("4"));
-            myDoubleLinkedList.append(CreateNode("11"), 1);
-            myDoubleLinkedList.appendEnd(CreateNode("8"));
-            myDoubleLinkedList.print();
-            myDoubleLinkedList.remove(5);
-            myDoubleLinkedList.print();
-
+            else if (testing == 4) 
+            { 
+            
+            }
+            
             //used to create node instances to add data to structures that use nodes
             Node CreateNode(string InputData) 
             {
